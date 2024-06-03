85b63478c1693395228d69603857f13562a97f3e
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-03 09:55:34 +0400

    Update page.tsx

--- src/app/(main)/(pages)/workflows/editor/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/page.tsx
@@ -1,9 +1,12 @@
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
 
 const Page = (props: Props) => {
   //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
+
+  redirect('/workflows/')
   return <div>Page</div>
 }
 
