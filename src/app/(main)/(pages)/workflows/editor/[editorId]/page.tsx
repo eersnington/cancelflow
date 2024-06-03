f202d5c8ef87a68f171afab5185077c51f822611
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-03 08:21:52 +0400

    Update page.tsx

--- src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -2,12 +2,24 @@ import { ConnectionsProvider } from '@/providers/connections-provider'
 import EditorProvider from '@/providers/editor-provider'
 import React from 'react'
 import EditorCanvas from './_components/editor-canvas'
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from 'next/navigation'
 
 type Props = {}
 
 const Page = (props: Props) => {
+
+  const { userId } = auth();
+
+  if (userId) {
+    console.log('userId', userId);
+  }
+
+  console.log(props);
+
   return (
     <div className="h-full">
+
       <EditorProvider>
         <ConnectionsProvider>
           <EditorCanvas />
