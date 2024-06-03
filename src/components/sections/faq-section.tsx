8e278a03e468b8a136291335c4db43dba861cfda
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-03 05:07:05 +0400

    Update faq-section.tsx

--- src/components/sections/faq-section.tsx
+++ src/components/sections/faq-section.tsx
@@ -8,13 +8,13 @@ import { faqData } from "@/lib/constant";
 
 export function FAQSection() {
     return (
-        <section className="container max-w-7xl py-16 mx-auto" id="faq">
+        <section className="container mt-8 max-w-7xl py-16 mx-auto" id="faq">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
                 <div className="md:col-span-1">
-                    <p className="bg-gradient-to-br from-neutral-700 to-black py-4 bg-clip-text text-left text-4xl md:text-7xl font-medium tracking-tight text-transparent">
+                    <p className="text-black dark:text-white py-4 text-left text-4xl md:text-7xl font-medium tracking-tight">
                         Frequently Asked Questions
                     </p>
-                    <p className="text-xl font-normal mt-4 text-left">
+                    <p className="text-black dark:text-white text-xl font-normal mt-4 text-left">
                         Get answers to common questions about Cancelflow.
                     </p>
                 </div>
