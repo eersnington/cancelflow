9b01a78970132fdca7e3b17f07286acdcd304455
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-06 09:24:03 +0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,9 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/clerk-webhook',
-    '/api/drive-activity/notification',
-    '/api/payment/success',
+    '/api/payment/webhook',
   ],
-  ignoredRoutes: [
-    '/api/auth/callback/discord',
-    '/api/auth/callback/notion',
-    '/api/auth/callback/slack',
-    '/api/flow',
-    '/api/cron/wait',
-  ],
+  ignoredRoutes: [],
 })
 
 export const config = {
