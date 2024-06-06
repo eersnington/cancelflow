a8995c2eefd2f8df071e4cc7e06b2fdec2b6f43c
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-06 09:34:08 +0400

    Update index.tsx

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/components/ui/tooltip'
 import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
-import Image from 'next/image'
+
 import { Separator } from '@/components/ui/separator'
 import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
 import { ModeToggle } from '../global/mode-toggle'
