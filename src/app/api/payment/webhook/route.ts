fec8c71a67867c5a2de32be9177193cb237c0343
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-07 18:26:17 +0400

    Update route.ts

--- src/app/api/payment/webhook/route.ts
+++ src/app/api/payment/webhook/route.ts
@@ -35,11 +35,12 @@ export async function POST(req: Request) {
 
     try {
         // Catch the event type
-        console.log(body);
+        // console.log(body);
         console.log("🍋 Event type -", eventType)
 
         if (eventType === "subscription_created") {
-            const { status, user_email, renews_at, product_name, customer_id, product_id, variant_id, first_subscription_item } = body.data.attributes;
+            const { id } = body.data;
+            const { status, user_email, renews_at, product_name, customer_id, product_id, variant_id } = body.data.attributes;
 
             const endDate = new Date(renews_at);
 
@@ -76,14 +77,14 @@ export async function POST(req: Request) {
 
             await db.subscription.upsert({
                 where: {
-                    lemonCustomerId: customer_id
+                    userEmail: user_email
                 },
                 update: {
-                    userEmail: user_email,
-                    lemonSubscriptionStatus: status,
+                    lemonCustomerId: customer_id,
                     lemonProductId: product_id,
                     lemonVariantId: variant_id,
-                    lemonSubscriptionId: first_subscription_item.subscription_id,
+                    lemonSubscriptionId: Number(id),
+                    lemonSubscriptionStatus: status,
                 },
                 create: {
                     user: {
@@ -95,7 +96,7 @@ export async function POST(req: Request) {
                     lemonCustomerId: customer_id,
                     lemonProductId: product_id,
                     lemonVariantId: variant_id,
-                    lemonSubscriptionId: first_subscription_item.subscription_id,
+                    lemonSubscriptionId: Number(id),
                     lemonSubscriptionStatus: status,
                 },
             });
@@ -108,14 +109,113 @@ export async function POST(req: Request) {
 
         } else if (eventType === "subscription_updated") {
 
-            const { user_email } = body.data.attributes;
+            const { id } = body.data;
+            const { user_email, status, renews_at, customer_id, product_id, variant_id } = body.data.attributes;
+            const endDate = new Date(renews_at);
 
-            console.log("Subscription updated -", user_email, status)
+            const user = await db.user.findUnique({
+                where: {
+                    email: user_email,
+                },
+            });
+            if (!user) {
+                return new NextResponse(`User not found - ${user_email}`, {
+                    status: 404,
+                })
+            }
+            // update subscription data in Subscription table
+
+            await db.subscription.update({
+                where: {
+                    userEmail: user_email,
+                },
+                data: {
+                    lemonCustomerId: customer_id,
+                    lemonProductId: product_id,
+                    lemonVariantId: variant_id,
+                    lemonSubscriptionId: Number(id),
+                    lemonSubscriptionStatus: status,
+                },
+            });
+
+            console.log("✅ Subscription updated -", user_email)
+
+            return new NextResponse(`Subscription plan updated for ${user_email}!`, {
+                status: 200,
+            })
+
+        } else if (eventType === "subscription_renewed") {
+
         } else if (eventType === "subscription_cancelled") {
             // Handle subscription deleted
             const { user_email } = body.data.attributes;
 
-            console.log("Subscription cancelled -", user_email, status)
+            const user = await db.user.findUnique({
+                where: {
+                    email: user_email,
+                },
+            });
+
+            if (!user) {
+                return new NextResponse(`User not found - ${user_email}`, {
+                    status: 404,
+                })
+            }
+
+            const subscription_end_date = user.tierEndDate;
+
+            await db.subscription.update({
+                where: {
+                    userEmail: user_email,
+                },
+                data: {
+                    lemonSubscriptionStatus: "cancelled",
+                },
+            });
+
+            console.log("Subscription cancelled -", user_email)
+
+            return new NextResponse(`Subscription plan cancelled for ${user_email}! Access retained till ${subscription_end_date}`, {
+                status: 200,
+            })
+        } else if (eventType === "subscription_expired") {
+            const { user_email } = body.data.attributes;
+
+            const user = await db.user.findUnique({
+                where: {
+                    email: user_email,
+                },
+            });
+
+            if (!user) {
+                return new NextResponse(`User not found - ${user_email}`, {
+                    status: 404,
+                })
+            }
+
+            await db.subscription.delete({
+                where: {
+                    userEmail: user_email,
+                }
+            });
+
+            await db.user.update({
+                where: {
+                    email: user_email,
+                },
+                data: {
+                    tier: "Starter",
+                    formLimit: "1",
+                    entryLimit: "50",
+                    tierEndDate: new Date(Date.now()),
+                },
+            });
+
+            console.log("Subscription expired -", user_email)
+
+            return new NextResponse(`Subscription plan expired for ${user_email}!`, {
+                status: 200,
+            })
         }
 
     } catch (err) {
