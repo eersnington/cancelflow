8fe65a27fd8349cbadcb17b1d1ce5b6e5a7d99ea
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-07 19:32:25 +0400

    Create paymentActions.ts

---
+++ src/actions/paymentActions.ts
@@ -0,0 +1,120 @@
+import { db } from "@/lib/db";
+import { planUpdateAction, subscriptionUpdateAction } from "@/lib/types";
+
+export async function updateUserPlan({
+    product_name, user_email, subscription_end_date }
+    : planUpdateAction) {
+
+    try {
+        if (product_name === 'Cancelflow - Plus') {
+            await db.user.update({
+                where: {
+                    email: user_email,
+                },
+                data: {
+                    tier: "Plus",
+                    formLimit: "50",
+                    entryLimit: "Unlimited",
+                    tierEndDate: subscription_end_date,
+                },
+            });
+
+        } else if (product_name === 'Cancelflow - Business') {
+
+            await db.user.update({
+                where: {
+                    email: user_email,
+                },
+                data: {
+                    tier: "Business",
+                    formLimit: "Unlimited",
+                    entryLimit: "Unlimited",
+                    tierEndDate: subscription_end_date,
+                },
+            });
+        }
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export async function updateUserSubcription({ user_email, id, customer_id, product_id, variant_id, status }: subscriptionUpdateAction) {
+    try {
+        await db.subscription.upsert({
+            where: {
+                userEmail: user_email
+            },
+            update: {
+                lemonCustomerId: customer_id,
+                lemonProductId: product_id,
+                lemonVariantId: variant_id,
+                lemonSubscriptionId: Number(id),
+                lemonSubscriptionStatus: status,
+            },
+            create: {
+                user: {
+                    connect: {
+                        email: user_email,
+                    },
+                },
+                userEmail: user_email,
+                lemonCustomerId: customer_id,
+                lemonProductId: product_id,
+                lemonVariantId: variant_id,
+                lemonSubscriptionId: Number(id),
+                lemonSubscriptionStatus: status,
+            },
+        });
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+
+export async function cancelSubscription({ user_email }: { user_email: string }) {
+    try {
+        await db.subscription.update({
+            where: {
+                userEmail: user_email,
+            },
+            data: {
+                lemonSubscriptionStatus: "cancelled",
+            },
+        });
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export async function deleteSubscription({ user_email }: { user_email: string }) {
+    try {
+
+        await db.subscription.delete({
+            where: {
+                userEmail: user_email,
+            }
+        });
+
+        await db.user.update({
+            where: {
+                email: user_email,
+            },
+            data: {
+                tier: "Starter",
+                formLimit: "1",
+                entryLimit: "50",
+                tierEndDate: new Date(Date.now()),
+            },
+        });
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
