3c87c85c5a9f7abc90636d9314527f5903732140
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 08:51:36 +0400

    Update page.tsx

--- src/app/(main)/(pages)/dnd/page.tsx
+++ src/app/(main)/(pages)/dnd/page.tsx
@@ -1,6 +1,9 @@
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import CreateFormBtn from './_components/create-form'
+import FormSkeleton from './_components/form-skeleton'
+import FormCard from './_components/form-card'
+import { GetForms } from '@/actions/form'
 
 type Props = {}
 
@@ -10,9 +13,29 @@ const Page = (props: Props) => {
             <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
                 Drag N Drop
             </h1>
-            <CreateFormBtn />
+            <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <CreateFormBtn />
+                <Suspense
+                    fallback={[1, 2, 3].map((el) => (
+                        <FormSkeleton key={el} />
+                    ))}
+                >
+                    <FormCards />
+                </Suspense>
+            </div>
         </div>
     )
 }
 
 export default Page
+
+async function FormCards() {
+    const forms = await GetForms();
+    return (
+        <>
+            {forms.map((form) => (
+                <FormCard key={form.id} form={form} />
+            ))}
+        </>
+    );
+}
