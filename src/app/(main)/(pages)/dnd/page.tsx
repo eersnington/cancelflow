fa3dd2b6a42429697f14ab30af2741c96392150c
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 07:19:44 +0400

    Update page.tsx

--- src/app/(main)/(pages)/dnd/page.tsx
+++ src/app/(main)/(pages)/dnd/page.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import CreateFormBtn from './_components/create-form'
 
 type Props = {}
 
@@ -9,7 +10,7 @@ const Page = (props: Props) => {
             <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
                 Drag N Drop
             </h1>
-            
+            <CreateFormBtn />
         </div>
     )
 }
