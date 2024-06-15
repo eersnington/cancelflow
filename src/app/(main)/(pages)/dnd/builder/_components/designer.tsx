895a8c6d859af07259c9c5fd237d7c7397917103
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 16:59:59 +0400

    Create designer.tsx

---
+++ src/app/(main)/(pages)/dnd/builder/_components/designer.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import { DragEndEvent, useDndMonitor, useDraggable, useDroppable } from "@dnd-kit/core";
+import { cn } from "@/lib/utils";
+
+function Designer() {
+
+    const droppable = useDroppable({
+        id: "designer-drop-area",
+        data: {
+            isDesignerDropArea: true,
+        },
+    });
+
+    return (
+        <div className="flex w-full h-full">
+            <div className="p-4 w-full">
+                <div className="dark:bg-white bg-black  max-w-[920px] mx-auto h-full rounded-xl
+                flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
+                    <p className="text-3xl font-bold flex flex-grow items-center text-white dark:text-black">
+                        Drop Here
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Designer;
