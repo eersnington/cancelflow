a5d4c2f47fa77c3d536cb86adc4b0dfd050e62fd
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 16:59:56 +0400

    Create designer-sidebar.tsx

---
+++ src/app/(main)/(pages)/dnd/builder/_components/designer-sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+
+function DesignerSidebar() {
+
+    return (
+        <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2
+        border-l-2">
+
+        </aside>
+    );
+}
+
+export default DesignerSidebar;
