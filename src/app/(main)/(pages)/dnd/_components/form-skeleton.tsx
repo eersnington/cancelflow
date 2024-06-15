f1fdf9a5a4362931ed04661fe2f53c52f17b6a8e
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 08:51:48 +0400

    Create form-skeleton.tsx

---
+++ src/app/(main)/(pages)/dnd/_components/form-skeleton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+const FormSkeleton = () => {
+    return (
+        <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />
+    );
+}
+
+export default FormSkeleton;
