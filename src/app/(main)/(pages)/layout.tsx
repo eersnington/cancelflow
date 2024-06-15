ca63d085823a3bd0628fbf616bf3ebd00d98b785
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-15 07:19:41 +0400

    Update layout.tsx

--- src/app/(main)/(pages)/layout.tsx
+++ src/app/(main)/(pages)/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 type Props = { children: React.ReactNode }
@@ -5,6 +6,7 @@ type Props = { children: React.ReactNode }
 const Layout = ({ children }: Props) => {
   return (
     <div className="border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl border-muted-foreground/20 overflow-scroll ">
+      <Toaster />
       {children}
     </div>
   )
