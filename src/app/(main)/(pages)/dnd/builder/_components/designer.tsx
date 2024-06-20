275fb8d9df1a12ec41cd5942c94f914c9a6d0531
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-20 07:54:11 +0400

    Update designer.tsx

--- src/app/(main)/(pages)/dnd/builder/_components/designer.tsx
+++ src/app/(main)/(pages)/dnd/builder/_components/designer.tsx
@@ -17,9 +17,9 @@ function Designer() {
     return (
         <div className="flex w-full h-full">
             <div className="p-4 w-full">
-                <div className="dark:bg-white bg-black  max-w-[920px] mx-auto h-full rounded-xl
+                <div className="bg-white dark:bg-black border-2 border-muted  max-w-[920px] mx-auto h-full rounded-xl
                 flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
-                    <p className="text-3xl font-bold flex flex-grow items-center text-white dark:text-black">
+                    <p className="text-3xl font-bold flex flex-grow items-center">
                         Drop Here
                     </p>
                 </div>
