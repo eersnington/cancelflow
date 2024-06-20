bef95e660fd1df2f024369e6db030aa43ff486ea
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-20 07:54:20 +0400

    Update text-field.tsx

--- src/components/fields/text-field.tsx
+++ src/components/fields/text-field.tsx
@@ -6,6 +6,20 @@ const type: ElementsType = "TextField";
 
 export const TextFieldFormElement: FormElement = {
     type,
+    constructor: (id: string) => ({
+        id,
+        type,
+        extraAttributes: {
+            label: "Text Field",
+            helperText: "This is a text field",
+            placeholder: "Enter your text here",
+            required: false,
+        },
+    }),
+    designerBtnElement: {
+        icon: <div>TextField</div>,
+        label: "Text Field",
+    },
     designerComponent: () => <div>TextField</div>,
     formComponent: () => <div>TextField</div>,
     propertiesComponent: () => <div>TextField</div>,
