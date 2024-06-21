46f78ee0c6b26b487591d9981a118ac42b6be287
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-21 07:55:13 +0400

    Update designer-sidebar.tsx

--- src/app/(main)/(pages)/dnd/builder/_components/designer-sidebar.tsx
+++ src/app/(main)/(pages)/dnd/builder/_components/designer-sidebar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import { cn } from "@/lib/utils";
+import SidebarBtnElement from "./sidebar-btn-element";
+import { FormElements } from "./form-elements";
 
 function DesignerSidebar() {
 
@@ -9,6 +10,7 @@ function DesignerSidebar() {
         <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2
         border-l-2 border-muted p-4 bg-white dark:bg-black overflow-y-auto h-full">
             <h1 className="text-2xl font-bold">Components</h1>
+            <SidebarBtnElement formElement={FormElements.TextField} />
         </aside>
     );
 }
